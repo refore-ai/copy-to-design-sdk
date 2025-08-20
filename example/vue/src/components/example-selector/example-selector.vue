e134448a7590e2e065ac99fe60dfb94ecf14ac17
Xinyu <123976670+NotCheer@users.noreply.github.com>
2025-08-20 10:40:45 +0800

    fix(vue-example): adjust the style of example selector and tab switcher (#12)

--- example/vue/src/components/example-selector/example-selector.vue
+++ example/vue/src/components/example-selector/example-selector.vue
@@ -5,41 +5,40 @@ import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuIte
 import { ChevronDown } from 'lucide-vue-next'
 
 defineProps<{
-  inputs: ExampleInput[]
-  modelValue: number
-}>()
+  inputs: ExampleInput[];
+  modelValue: number;
+}>();
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: number): void
-}>()
+  (e: 'update:modelValue', value: number): void;
+}>();
 
-const isOpen = ref(false)
+const isOpen = ref(false);
 
 const handleClick = (idx: number) => {
-  emit('update:modelValue', idx)
-  isOpen.value = false
-}
+  emit('update:modelValue', idx);
+  isOpen.value = false;
+};
 </script>
 
 <template>
   <DropdownMenu v-model:open="isOpen">
     <DropdownMenuTrigger
-        class="bg-background text-foreground rounded-lg border shadow px-2 py-1 text-sm flex items-center justify-between hover:bg-gray-50"
+      class="w-32 bg-background text-foreground rounded-lg border shadow px-2 py-1 text-sm flex items-center justify-between hover:bg-gray-50"
     >
-      {{ inputs[modelValue]?.label || 'Select' }}
+      <a class="ml-1 font-medium"> {{ inputs[modelValue]?.label || 'Select' }} </a>
       <ChevronDown
-          class="ml-2 h-3 w-3 text-gray-600 transition-transform duration-200"
-          :class="{ 'rotate-180 transform': isOpen }"
+        class="ml-2 h-3 w-3 text-gray-600 transition-transform duration-200"
+        :class="{ 'rotate-180 transform': isOpen }"
       />
     </DropdownMenuTrigger>
 
-    <DropdownMenuContent>
+    <DropdownMenuContent align="center">
       <DropdownMenuItem
-          v-for="(opt, idx) in inputs"
-          :key="idx"
-          class="h-6 rounded px-2 hover:bg-gray-50 mt-1"
-          :class="{ 'bg-gray-200': idx === modelValue }"
-          @click="handleClick(idx)"
+        v-for="(opt, idx) in inputs"
+        :key="idx"
+        :class="['h-6 rounded px-2 mt-1', idx === modelValue ? 'bg-gray-200 pointer-events-none' : 'hover:bg-gray-50']"
+        @click="handleClick(idx)"
       >
         {{ opt.label }}
       </DropdownMenuItem>
